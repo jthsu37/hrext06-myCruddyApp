2db97dea33cd439ed8e0d92afc72c535795e617d
john hsu <hsu@johns-Air.home>
2018-12-21 05:58:16 -0800

    finished current sprint

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 $(document).ready(function(){
-  console.log('jQuery loaded');
-
   // write to local storage from input when button save clicked
-  $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
+   $('.btn-submit').on('click', function(){
+    let key;
+    let value;
+    if ($('.input-key').val() && $('.input-value').val()) {
+      var arr = [];
+      if (!localStorage.hasOwnProperty($('.input-key').val())) {
+        localStorage.setItem($('.input-key').val(), $('.input-value').val())
+        key = $('.input-key').val()
+        value = $('.input-value').val()
+      } else if (localStorage.getItem($('.input-key').val()).includes($('.input-value').val())) {
+        key = $('.input-key').val()
+        value = localStorage.getItem($('.input-key').val())
+      } else {
+        var prev = localStorage.getItem($('.input-key').val())
+        var curr = $('.input-value').val()
+        arr.push(prev + ', ' + curr)
+        localStorage.setItem($('.input-key').val(), arr)
+        key = $('.input-key').val()
+        value = arr;
+      }
+    }
 
     // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
-
+      let $keyValue = $('<div class="keys">' + key + ': ' + '</div><div class="value">' + value + '</div>')
+      $('.list-display-field').html(' ')
+      $('.list-display-field').append($keyValue)
   });
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    localStorage.removeItem($('.input-key').val());
+    $('.list-display-field').text('nothing to display yet');
   });
-
+  $('.btn-delete-all').on('click', function(){
+    localStorage.clear()
+    $('.list-display-field').text('nothing to display yet');
+  });
 });
